86dcf861d9bbc818e143bd6e95ae5aafabe8dac4
An Nguyen <19521185@gm.uit.edu.vn>
2023-11-26 13:58:45 +0700

    [26/11] Fixing bug when signing out

--- resources/js/src/layouts/header.jsx
+++ resources/js/src/layouts/header.jsx
@@ -38,7 +38,7 @@ function Header(props) {
 
     const { variant } = props;
 
-    console.log(user.permissions?.includes("monitor"))
+    // console.log(user.permissions?.includes("monitor"))
 
     const handleSignOut = async () => {
         try {
@@ -123,7 +123,7 @@ function Header(props) {
                                 </li>
                             )}
                         </NavLink>
-                        {user.permissions?.includes("monitor") && (<NavLink to="/integration" className="flex items-center">
+                        {user?.permissions?.includes("monitor") && (<NavLink to="/integration" className="flex items-center">
                             {({ isActive }) => (
                                 <li
                                     className={
@@ -230,7 +230,7 @@ function Header(props) {
                                                 Quản lý người dùng
                                             </MenuItem>
                                         </Link>
-                                        {user.permissions?.includes("monitor") && (<Link to="/integration">
+                                        {user?.permissions?.includes("monitor") && (<Link to="/integration">
                                             <MenuItem
                                                 icon={
                                                     <SiSap className="text-2xl"/>
