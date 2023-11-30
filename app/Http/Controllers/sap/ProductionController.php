dce27dde974f369f04b70674d387cc3fe4a8b662
harrydevnguyen <tnguyen.nguyen@vn.gt.com>
2023-12-01 05:55:32 +0700

    update package horizon

--- app/Http/Controllers/sap/ProductionController.php
+++ app/Http/Controllers/sap/ProductionController.php
@@ -44,4 +44,33 @@ class ProductionController extends Controller
             'data' => $request->all()
         ], 200);
     }
+    function allocate()
+    {
+        $data = [
+            ["Production" => 10, "Qty" => 10, "input" => null],
+            ["Production" => 12, "Qty" => 20, "input" => null]
+        ];
+        $totalQty = 25;
+
+        foreach ($data as &$item) {
+            // Sử dụng isset() thay vì so sánh với phần tử đầu tiên trong mảng
+            if (isset($item['Qty']) && $item['Qty'] <= $totalQty) {
+                $item['input'] = $item['Qty'];
+                $totalQty -= $item['Qty'];
+            } else {
+                // Chỉ cập nhật giá trị nếu Qty lớn hơn 0
+                if ($item['Qty'] > 0) {
+                    $item['input'] = min($item['Qty'], $totalQty);
+                    $totalQty -= $item['input'];
+                } else {
+                    $item['input'] = 0;
+                }
+            }
+        }
+
+        // Sử dụng array_filter với callback ngắn gọn hơn
+        $filteredData = array_filter($data, fn ($item) => $item['input'] != 0);
+
+        return response()->json(['data' => array_values($filteredData)]);
+    }
 }
