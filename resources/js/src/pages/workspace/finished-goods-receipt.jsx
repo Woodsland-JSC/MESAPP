e9773b5ab6f057bacb5d0e0c5c7db60611389f3a
An Nguyen <19521185@gm.uit.edu.vn>
2023-12-02 13:03:48 +0700

    [02/12] Nhập thành phẩm update 3

--- resources/js/src/pages/workspace/finished-goods-receipt.jsx
+++ resources/js/src/pages/workspace/finished-goods-receipt.jsx
@@ -955,13 +955,42 @@ function FinishedGoodsReceipt() {
         console.log("Ra example data: ", exampleData);
     };
 
-    const onFilterTextBoxChanged = async (value) => {
-        console.log(value);
+    const onFilterTextBoxChanged = async (e) => {
+        const input = e.target.value;
+        if (!input) {
+            if (selectedGroup) {
+                if (selectedGroup.value == "TH-X3SC") {
+                    setCurrentData(exampleData);
+                } else if (selectedGroup.value == "TH-X3TC1") {
+                    setCurrentData(exampleData1);
+                } else if (selectedGroup.value == "TH-X3TC2") {
+                    setCurrentData(exampleData2);
+                } else {
+                    setCurrentData([]);
+                }
+            }
+            return;
+        }
+        if (input) {
+            var result = currentData.filter((item) => {
+                if (item.itemName.toLowerCase().includes(input.toLowerCase())) {
+                    return true;
+                }
+            
+                const hasSubItem = item.itemDetails.some(
+                    (detail) => detail.subItemName.toLowerCase().includes(input.toLowerCase())
+                );
+            
+                return hasSubItem;
+            });
+
+            setCurrentData(result);
+        }
     };
 
-    useEffect(() => {
-        console.log("Log phôi chờ: ", awaitingReception);
-    }, [awaitingReception]);
+    // useEffect(() => {
+    //     console.log("Log phôi chờ: ", awaitingReception);
+    // }, [awaitingReception]);
 
     useEffect(() => {
         // const getFinishedGoods = async () => {
