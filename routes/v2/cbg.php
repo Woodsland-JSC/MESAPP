9cf8add8fc1f0b6c0d3b3b8e1f65d92038f47e2a
ntnfit <ntnfit98@gmail.com>
2024-07-15 18:32:18 +0700

    remove v2 che bien go va receipt_bk

--- routes/v2/cbg.php
+++ routes/v2/cbg.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\sap\ProductionController;
 // route cho api che bien go
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::group(['prefix' => 'v2/production'], function () {
-        Route::get('/receipts-productions', [ProductionController::class, 'indexV2']);
-        Route::get('/receipts-productions-detail', [ProductionController::class, 'viewdetailV2']);
-        Route::post('/accept-receipts', [ProductionController::class, 'acceptV2']);
+        // Route::get('/receipts-productions', [ProductionController::class, 'indexV2']);
+        // Route::get('/receipts-productions-detail', [ProductionController::class, 'viewdetailV2']);
+        // Route::post('/accept-receipts', [ProductionController::class, 'acceptV2']);
     
     });
 });
