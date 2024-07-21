34c5b1a938813297eafa6a5fa6b72401ee864661
ntnfit <ntnfit98@gmail.com>
2024-07-21 16:36:30 +0700

    route v2 cbg&vcn

--- routes/v2/cbg.php
+++ routes/v2/cbg.php
@@ -2,11 +2,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\sap\ProductionController;
+use App\Http\Controllers\sap\VCNController;
 // route cho api che bien go
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::group(['prefix' => 'v2/production'], function () {
         Route::post('/accept-receipts', [ProductionController::class, 'accept_v2']);
-        route::get('/get-test', [ProductionController::class, 'collectStockAllocate']);
+        route::get('/confirm-qc-cbg', [ProductionController::class, 'AcceptQCCBG_v2']);
     
+    });
+    Route::group(['prefix' => 'v2/vcn'], function () {
+        Route::post('/accept-receipts', [VCNController::class, 'accept_v2']);
+        route::get('/confirm-qc-vcn', [VCNController::class, 'AcceptQCVCN_v2']);
     });
 });
