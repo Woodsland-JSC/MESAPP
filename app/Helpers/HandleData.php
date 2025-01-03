b1c9961f22720d952b7d607f4815295e601987ef
Pedro Scott <pedscottdev@gmail.com>
2025-01-03 16:18:15 +0700

    Hot fix

--- app/Helpers/HandleData.php
+++ app/Helpers/HandleData.php
@@ -44,104 +44,104 @@ if (!function_exists('playloadBatch')) {
         return  ['payload' => $output, 'uid' => $uid];
     }
 }
-if (!function_exists('playloadIssueCBG')) {
-    function playloadIssueCBG($ItemCode, $Qty, $WarehouseCode, $branch)
-    {
-        $conDB = (new ConnectController)->connect_sap();
+    if (!function_exists('playloadIssueCBG')) {
+        function playloadIssueCBG($ItemCode, $Qty, $WarehouseCode, $branch)
+        {
+            $conDB = (new ConnectController)->connect_sap();
 
-        $query = 'select * from "Uf_CheckStockByWhsCode_ManBy"(?,?)';
-        $stmt = odbc_prepare($conDB, $query);
-        if (!$stmt) {
-            throw new \Exception('Error preparing SQL statement: ' . odbc_errormsg($conDB));
-        }
-        if (!odbc_execute($stmt, [$ItemCode, $WarehouseCode])) {
-            throw new \Exception('Error executing SQL statement: ' . odbc_errormsg($conDB));
-        }
-        // Fetch the single record
-        $row = odbc_fetch_array($stmt);
+            $query = 'select * from "Uf_CheckStockByWhsCode_ManBy"(?,?)';
+            $stmt = odbc_prepare($conDB, $query);
+            if (!$stmt) {
+                throw new \Exception('Error preparing SQL statement: ' . odbc_errormsg($conDB));
+            }
+            if (!odbc_execute($stmt, [$ItemCode, $WarehouseCode])) {
+                throw new \Exception('Error executing SQL statement: ' . odbc_errormsg($conDB));
+            }
+            // Fetch the single record
+            $row = odbc_fetch_array($stmt);
 
-        if ($row === false) {
-            throw new \Exception('No data returned.');
-        }
-        // Extract the desired values
-        $itemType = $row['ItemType'];
-        $stock = $row['Stock'];
-        if ($stock < $Qty) {
-            throw new \Exception('Số lượng tồn ' + $ItemCode + ' tại kho ' + $WarehouseCode + ' không đủ để xuất.');
-        }
-        //2.allocate data
-        $query = 'call "USP_AllocateIssue"(?,?,?,?)';
-        $stmt = odbc_prepare($conDB, $query);
-        if (!$stmt) {
-            throw new \Exception('Error preparing SQL statement: ' . odbc_errormsg($conDB));
-        }
-        if (!odbc_execute($stmt, [$ItemCode, $WarehouseCode, $Qty, (int)$itemType])) {
-            throw new \Exception('Error executing SQL statement: ' . odbc_errormsg($conDB));
-        }
-        $results = array();
-        while ($row = odbc_fetch_array($stmt)) {
-            $results[] = $row;
-        }
+            if ($row === false) {
+                throw new \Exception('No data returned.');
+            }
+            // Extract the desired values
+            $itemType = $row['ItemType'];
+            $stock = $row['Stock'];
+            if ($stock < $Qty) {
+                throw new \Exception('Số lượng tồn ' . $ItemCode . ' tại kho ' . $WarehouseCode . ' không đủ để xuất.');
+            }
+            //2.allocate data
+            $query = 'call "USP_AllocateIssue"(?,?,?,?)';
+            $stmt = odbc_prepare($conDB, $query);
+            if (!$stmt) {
+                throw new \Exception('Error preparing SQL statement: ' . odbc_errormsg($conDB));
+            }
+            if (!odbc_execute($stmt, [$ItemCode, $WarehouseCode, $Qty, (int)$itemType])) {
+                throw new \Exception('Error executing SQL statement: ' . odbc_errormsg($conDB));
+            }
+            $results = array();
+            while ($row = odbc_fetch_array($stmt)) {
+                $results[] = $row;
+            }
 
-        //3. create issue production
-        if (count($results) == 0) {
-            throw new \Exception('Error creating issue production');
-        }
-        $serial_batch = [];
-        foreach ($results as $result) {
-            if ($itemType == 1) {
-                $serial_batch[] = [
-                    'BatchNumber' => $result['BatchSeri'],
-                    'Quantity' => $result['Allocated']
+            //3. create issue production
+            if (count($results) == 0) {
+                throw new \Exception('Error creating issue production');
+            }
+            $serial_batch = [];
+            foreach ($results as $result) {
+                if ($itemType == 1) {
+                    $serial_batch[] = [
+                        'BatchNumber' => $result['BatchSeri'],
+                        'Quantity' => $result['Allocated']
+                    ];
+                }
+                if ($itemType == 2) {
+                    $serial_batch[] = [
+                        'SystemSerialNumber' => $result['BatchSeri'],
+                        'Quantity' => $result['Allocated']
+                    ];
+                }
+            }
+            if ($itemType == 0) {
+                $body = [
+                    "BPL_IDAssignedToInvoice" =>  Auth::user()->branch,
+                    "DocumentLines" => [
+                        [
+                            "ItemCode" => $ItemCode,
+                            "Quantity" => $Qty,
+                            "WarehouseCode" => $WarehouseCode,
+                        ]
+                    ]
+                ];
+            } else if ($itemType == 1) {
+                $body = [
+                    "BPL_IDAssignedToInvoice" =>  Auth::user()->branch,
+                    "DocumentLines" => [
+                        [
+                            "ItemCode" => $ItemCode,
+                            "Quantity" => $Qty,
+                            "WarehouseCode" => $WarehouseCode,
+                            "BatchNumbers" =>  $serial_batch
+                        ]
+                    ]
+                ];
+            } else if ($itemType == 2) {
+                $body = [
+                    "BPL_IDAssignedToInvoice" => Auth::user()->branch,
+                    "DocumentLines" => [
+                        [
+                            "ItemCode" => $ItemCode,
+                            "Quantity" => $Qty,
+                            "WarehouseCode" => $WarehouseCode,
+                            "SerialNumbers" => $serial_batch
+                        ]
+                    ]
                 ];
             }
-            if ($itemType == 2) {
-                $serial_batch[] = [
-                    'SystemSerialNumber' => $result['BatchSeri'],
-                    'Quantity' => $result['Allocated']
-                ];
-            }
-        }
-        if ($itemType == 0) {
-            $body = [
-                "BPL_IDAssignedToInvoice" =>  Auth::user()->branch,
-                "DocumentLines" => [
-                    [
-                        "ItemCode" => $ItemCode,
-                        "Quantity" => $Qty,
-                        "WarehouseCode" => $WarehouseCode,
-                    ]
-                ]
-            ];
-        } else if ($itemType == 1) {
-            $body = [
-                "BPL_IDAssignedToInvoice" =>  Auth::user()->branch,
-                "DocumentLines" => [
-                    [
-                        "ItemCode" => $ItemCode,
-                        "Quantity" => $Qty,
-                        "WarehouseCode" => $WarehouseCode,
-                        "BatchNumbers" =>  $serial_batch
-                    ]
-                ]
-            ];
-        } else if ($itemType == 2) {
-            $body = [
-                "BPL_IDAssignedToInvoice" => Auth::user()->branch,
-                "DocumentLines" => [
-                    [
-                        "ItemCode" => $ItemCode,
-                        "Quantity" => $Qty,
-                        "WarehouseCode" => $WarehouseCode,
-                        "SerialNumbers" => $serial_batch
-                    ]
-                ]
-            ];
-        }
 
-        return $body;
+            return $body;
+        }
     }
-}
 if (!function_exists('qtycachIemSAP')) {
     function qtycachIemSAP($stringItem)
     {
