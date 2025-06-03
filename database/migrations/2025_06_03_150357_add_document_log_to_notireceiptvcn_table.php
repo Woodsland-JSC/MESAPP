a7f7f270cf451d24682f9fe0d292372288a4182f
Pedro Scott <pedscottdev@gmail.com>
2025-06-03 15:35:52 +0700

    fix: case sensitivity on migration

--- database/migrations/2025_06_03_150357_add_document_log_to_notireceiptvcn_table.php
+++ database/migrations/2025_06_03_150357_add_document_log_to_notireceiptvcn_table.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::table('notireceiptvcn', function (Blueprint $table) {
+        Schema::table('notireceiptVCN', function (Blueprint $table) {
             $table->text('document_log')->nullable()->after('DocEntry');
         });
     }
 
     public function down(): void
     {
-        Schema::table('notireceiptvcn', function (Blueprint $table) {
+        Schema::table('notireceiptVCN', function (Blueprint $table) {
             $table->dropColumn('document_log');
         });
     }
