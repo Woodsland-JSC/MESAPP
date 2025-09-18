c373a02da1c782cd73de531afa39f1561849f1d4
Nguyễn Thanh Nguyên <58658850+ntnfit@users.noreply.github.com>
2025-09-18 09:49:58 +0700

    Update index.blade.php

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -65,9 +65,9 @@
                                     <pre class="whitespace-pre-wrap text-xs bg-gray-50 p-2 rounded">{{ Str::limit($job->payload, 200) }}</pre>
                                 </td>
                                 <td class="px-3 py-2 relative group">
-                                    <pre class="whitespace-pre-wrap text-xs text-red-600 bg-gray-50 p-2 rounded cursor-help">
-                                        {{ Str::limit($job->exception, 200) }}
-                                    </pre>
+                                    <div class="absolute z-10 hidden group-hover:block bg-black text-white text-xs p-2 rounded max-w-md">
+                                       {{ $job->exception }}
+                                    </div>
                                     <div class="absolute z-10 hidden group-hover:block bg-black text-white text-xs p-2 rounded max-w-md">
                                         {{ $job->exception }}
                                     </div>
