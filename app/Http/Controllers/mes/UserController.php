a9bdaa6310a6820ff09a579d9b0d822cbabc5fb5
Phạm Anh Tuấn <tuanpa@woodsland.vn>
2025-10-21 16:25:54 +0700

    commit

--- app/Http/Controllers/mes/UserController.php
+++ app/Http/Controllers/mes/UserController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers\mes;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
+use DB;
 use Exception;
 use Illuminate\Http\Request;
 
 
 class UserController extends Controller
-{ 
+{
     // Lấy danh sách thủ kho theo nhà máy.
-    public function danhSachThuKho(){
+    public function danhSachThuKho()
+    {
         try {
             $plantId = Auth::user()->plant;
-            $users  = User::query()
-                ->where('plant', '=', $plantId)
-                ->where('role', '=' , '')
+            $users = DB::table('users as u')
+                ->join('roles as r', 'u.role', '=', 'r.id')
+                ->join('role_has_permissions as rhp', 'r.id', '=', 'rhp.role_id')
+                ->join('permissions as p', 'rhp.permission_id', '=', 'p.id')
+                ->where('p.name', 'xacnhanlosay')
+                ->where('u.plant', $plantId)
+                ->select('u.*', 'p.*')
                 ->get();
 
             return response()->json([
