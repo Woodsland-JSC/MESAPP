3f1d1bac5c72926191c53af107cbd8a88261bb2a
Phạm Anh Tuấn <tuanpa@woodsland.vn>
2025-10-22 08:23:26 +0700

    update

--- app/Http/Controllers/mes/PlanDryingController.php
+++ app/Http/Controllers/mes/PlanDryingController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\planDryings as PlanDrying;
 use Auth;
 use Exception;
+use Log;
 use Validator;
 
 class PlanDryingController extends Controller
@@ -17,12 +18,7 @@ class PlanDryingController extends Controller
         try {
             $params = $request->only(['planId', 'receiverId']);
 
-            $validator = Validator::make($params, [
-                'planId' => 'require',
-                'receiverId' => 'require'
-            ]);
-
-            if ($validator->fails()) {
+            if (!$params['planId'] || !$params['receiverId']) {
                 return response()->json([
                     'message' => 'Thiếu dữ liệu!'
                 ], 500);
@@ -47,6 +43,7 @@ class PlanDryingController extends Controller
                 'data' => 'success'
             ]);
         } catch (Exception $e) {
+            Log::info($e->getMessage());
             return response()->json([
                 'message' => 'Chuyển cho người xác nhận có lỗi!'
             ], 500);
@@ -77,4 +74,30 @@ class PlanDryingController extends Controller
             ], 500);
         }
     }
+
+
+    // $results = planDryings::select(
+    //                 'planDryings.Code as newbatch',
+    //                 'planDryings.Oven',
+    //                 'pallets.DocEntry',
+    //                 'pallets.Code',
+    //                 'pallets.palletID',
+    //                 'pallets.palletSAP',
+    //                 'pallets.CompletedBy',
+    //                 'pallet_details.ItemCode',
+    //                 'pallet_details.WhsCode2',
+    //                 'pallet_details.Qty',
+    //                 'pallet_details.CDai',
+    //                 'pallet_details.CRong',
+    //                 'pallet_details.CDay',
+    //                 'pallet_details.BatchNum',
+    //                 DB::raw('SUM(pallet_details.Qty) OVER (PARTITION BY pallets.palletID) AS TotalQty'),
+    //                 'pallet_details.palletID'
+    //             )
+    //                 ->join('plan_detail', 'planDryings.PlanID', '=', 'plan_detail.PlanID')
+    //                 ->join('pallets', 'plan_detail.pallet', '=', 'pallets.palletID')
+    //                 ->join('pallet_details', 'pallets.palletID', '=', 'pallet_details.palletID')
+    //                 ->where('planDryings.PlanID', $id)
+    //                 ->whereNull('pallets.CompletedBy')
+    //                 ->get();
 }
