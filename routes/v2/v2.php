2c9e40ef9e196f129b63ab13160d09c4129837a7
Phạm Anh Tuấn <tuanpa@woodsland.vn>
2025-11-10 09:43:10 +0700

    ghi nhận VCN công đoạn RO

--- routes/v2/v2.php
+++ routes/v2/v2.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\sap\ProductionController;
 use App\Http\Controllers\sap\VCNController;
 use App\Http\Controllers\sap\QCController;
-// route cho api che bien go
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::group(['prefix' => 'v2/production'], function () {
         Route::post('/accept-receipts', [ProductionController::class, 'acceptV2']);
         route::post('/confirm-qc-cbg', [QCController::class, 'acceptTeamQCCBGV2']);
-    
     });
+
     Route::group(['prefix' => 'v2/vcn'], function () {
         Route::get('/receipts-productions', [VCNController::class, 'indexv2']);
         Route::post('/accept-receipts', [VCNController::class, 'acceptV2']);
